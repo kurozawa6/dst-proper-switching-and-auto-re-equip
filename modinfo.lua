ef512a5cd575351acbfc6af5848180916aff7fc5
kurozawa6 <75070724+kurozawa6@users.noreply.github.com>
2022-07-12 12:37:47 +0800

    Create modinfo.lua

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+name = "Proper Item Switching"
+description = ""
+
+author = "Growth Mindset"
+
+version = "1.0"
+
+forumthread = ""
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+
+server_only_mod = true
+client_only_mod = false -- THE GOAL
+all_clients_require_mod = false
+
+api_version = 10
