7ea10d935e45a443f1e64ff7d38201c2724f0f4c
kurozawa6 <kurozawa66@gmail.com>
2022-07-16 19:16:52 +0800

    conditional logic now working, to do equips table

--- test.lua
+++ test.lua
@@ -1,22 +1,31 @@
 local ENV = env
 GLOBAL.setfenv(1, GLOBAL)
 
-local latest_remove_slot = nil
+local latest_remove_slot = nil -- TO CHANGE TO TABLE?
 local latest_get_slot = nil
+local latest_get_item = nil
+local latest_equipped_item = nil
+local equipment_switched = false
 
 local function delay_again(inst, fn)
     inst:DoTaskInTime(0, fn)
 end
 
 local function ModOnEquip(inst, data)
-    if type(data) == "table" and data.eslot and data.item then
-        local item = data.item
-        print("ModOnEquip data.item:", item)
+    if not (type(data) == "table" and data.eslot and data.item) then return end
+    local item = data.item
+    local item_to_replace = nil
+    if latest_equipped_item then
+        item_to_replace = latest_equipped_item
     end
-    local function debug_print(_)
-        print(latest_remove_slot, latest_get_slot)
+    latest_equipped_item = item
+    --print("ModOnEquip data.item:", item)
+    local function update_equipped_item(_)
+        if item_to_replace and latest_get_item and item_to_replace == latest_get_item then
+            equipment_switched = true
+        end
     end
-    inst:DoTaskInTime(0, delay_again, debug_print)
+    inst:DoTaskInTime(0, update_equipped_item)
     --if equipslots slot is taken then record item on slot
     --equipslots slot = equipped item
     --if mod latest remove slot == nil or mod latest give slot == nil then return end
@@ -26,26 +35,35 @@ end
 
 local function ModOnUnequip(_, data)
     if type(data) ~= "table" then return end
-    local item = data.item
+    --local item = data.item
+    latest_equipped_item = nil
     --print(item)
     --remove item from equipslots
 end
 
-local function ModOnItemGet(inst, data)
+local function ModOnItemGet(_, data)
     --record to mod latest get slot
     local get_slot = data.slot
     local item = data.item
-    local function latest_update(_)
-        latest_get_slot = get_slot
-    end
-    inst:DoTaskInTime(0, latest_update)
-    print("ModOnItemGet data:", item, get_slot)
+    latest_get_slot = get_slot
+    latest_get_item = item
+    --local function latest_update(_)
+    --end
+    --inst:DoTaskInTime(0, latest_update)
+    --print("ModOnItemGet data:", item, get_slot)
 end
 
-local function ModOnItemLose(_, data) -- IMPORTANT EVENT FUNCTION THAT IS CALLED ONLY WHEN NEEDED! USE THIS! use separate remove_slot for every equipslot, terminate when item has no equipslot
+local function ModOnItemLose(inst, data) -- IMPORTANT EVENT FUNCTION THAT IS CALLED ONLY WHEN NEEDED! USE THIS! use separate remove_slot for every equipslot, terminate when item has no equipslot
     local remove_slot = data.slot
     latest_remove_slot = remove_slot
-    print("ModOnItemLose data.slot:", remove_slot)
+    --print("ModOnItemLose data:", remove_slot, item)
+    local function main_auto_switch()
+        if equipment_switched == true then
+            equipment_switched = false
+            print("Move", latest_get_item, "from", latest_get_slot, "to", latest_remove_slot) -- TO IMPLEMENT ACTUAL FUNCTION
+        end
+    end
+    inst:DoTaskInTime(0, delay_again, main_auto_switch)
     --record to mod latest remove slot
 end
 
